15748c9b1d7b2a28994bfdf50c356746ff6d8da7
ElenaCKay <elenakay1@googlemail.com>
2023-04-17 16:44:20 +0100

    added a link to main page and a button to go to the top

--- src/App.js
+++ src/App.js
@@ -3,10 +3,16 @@ import { Routes, Route } from "react-router-dom";
 import Header from "./components/header.jsx";
 import Reviews from "./components/reviews";
 import ReviewCard from "./components/reviewCard";
+import GoToTopButton from "./components/go-to-top-button";
+import { Link } from "react-router-dom";
+
 function App() {
     return (
         <div>
-            <Header />
+            <Link to="/">
+                <Header />
+            </Link>
+            <GoToTopButton />
             <Routes>
                 <Route path="/" element={<Reviews />} />
                 <Route path="/reviews" element={<Reviews />} />

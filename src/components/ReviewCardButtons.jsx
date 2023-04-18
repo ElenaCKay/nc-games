ad96d1066916d9341e84bccdbb669980e624f520
ElenaCKay <elenakay1@googlemail.com>
2023-04-18 16:07:15 +0100

    fixed so the review votes are optimistic

--- src/components/ReviewCardButtons.jsx
+++ src/components/ReviewCardButtons.jsx
@@ -1,28 +1,28 @@
-import { useState } from "react";
 import { patchReviewVotes } from "../api";
 
 const ReviewCardButtons = ({ review_id, setReviewVotes }) => {
-    const [incVotes, setIncVotes] = useState(0);
-
     const upVoteHandler = (effect) => {
-        setIncVotes(1);
-        patchReviewVotes(review_id, incVotes)
-            .then((data) => {
-                setReviewVotes(data);
-            })
-            .catch((err) => {
-                console.log(err);
+        setReviewVotes((reviewVotes) => {
+            return reviewVotes + 1;
+        });
+        patchReviewVotes(review_id, 1).catch((err) => {
+            console.log(err);
+            setReviewVotes((reviewVotes) => {
+                return reviewVotes - 1;
             });
+        });
     };
     const downVoteHandler = (effect) => {
-        setIncVotes(-1);
-        patchReviewVotes(review_id, incVotes)
-            .then((data) => {
-                setReviewVotes(data);
-            })
-            .catch((err) => {
-                console.log(err);
+        setReviewVotes((reviewVotes) => {
+            return reviewVotes - 1;
+        });
+
+        patchReviewVotes(review_id, -1).catch((err) => {
+            console.log(err);
+            setReviewVotes((reviewVotes) => {
+                return reviewVotes + 1;
             });
+        });
     };
 
     return (

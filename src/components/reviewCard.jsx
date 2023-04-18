6576faa88b3272cbd2eb0985b168e1a615105556
ElenaCKay <elenakay1@googlemail.com>
2023-04-18 14:26:38 +0100

    ticket 7 increase votes

--- src/components/reviewCard.jsx
+++ src/components/reviewCard.jsx
@@ -1,18 +1,38 @@
 import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
-import { fetchReviewById } from "../api";
+import { fetchReviewById, patchReviewVotes } from "../api";
 
 const ReviewCard = () => {
     const { review_id } = useParams();
     const [review, setReview] = useState({});
     const [reviewLoading, setreviewLoading] = useState(true);
+    const [reviewVotes, setReviewVotes] = useState(0);
+    const [incVotes, setIncVotes] = useState(0);
 
     useEffect(() => {
         fetchReviewById(review_id).then((data) => {
             setReview(data);
+            setReviewVotes(data.votes);
             setreviewLoading(false);
         });
     }, [review_id]);
+
+    useEffect(() => {
+        patchReviewVotes(review_id, incVotes)
+            .then((data) => {
+                setReviewVotes(data);
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    }, [review_id, incVotes]);
+
+    const upVoteHandler = (effect) => {
+        setIncVotes(1);
+    };
+    const downVoteHandler = (effect) => {
+        setIncVotes(-1);
+    };
 
     if (reviewLoading) {
         return <h3>Loading...</h3>;
@@ -28,10 +48,10 @@ const ReviewCard = () => {
                         {review.review_body} <br></br>
                         {review.created_at}
                     </p>
-                    <h4>Votes: {review.votes}</h4>
+                    <h4>Votes: {reviewVotes}</h4>
                 </section>
-                <button>Up Vote</button>
-                <button>Down Vote</button>
+                <button onClick={upVoteHandler}>Up Vote</button>
+                <button onClick={downVoteHandler}>Down Vote</button>
                 <button>Comments</button>
             </div>
         );
